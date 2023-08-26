cc856801bfb4ae16272974a9b63261943b99e356
lifegpc <g1710431395@gmail.com>
2023-08-26 16:22:32 +0800

    Fix plugin-twind-main.js not build

--- server-dev.ts
+++ server-dev.ts
@@ -1,3 +1,7 @@
 import dev from "$fresh/dev.ts";
+import twindPlugin from "$fresh/plugins/twind.ts";
+import twindConfig from "./twind.config.ts";
 
-await dev(import.meta.url, "./server-run.ts");
+await dev(import.meta.url, "./server-run.ts", {
+    plugins: [twindPlugin(twindConfig)],
+});
