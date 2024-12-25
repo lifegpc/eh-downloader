5131515770d2e6a22cacff28d03f1091292dd408
lifegpc <root@lifegpc.com>
2024-12-25 15:48:01 +0800

    fix: Set ownership for deno-dir in Docker entrypoint script

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -6,5 +6,6 @@ if [ $? -ne 0 ]; then
         su root -c "groupadd -g $(id -g) eh"
     fi
     su root -c "useradd -g $(id -g) -m -s /bin/bash -u $(id -u) eh"
+    su root -c "chown -R eh /deno-dir"
 fi
 deno task server
